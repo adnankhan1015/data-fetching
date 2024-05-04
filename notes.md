708ffdb98ca9a13ec0d83567f24eafe98b2d2e2d
adnankhan <adnankhannad@gmail.com>
2024-05-04 18:55:51 +0500

    fetch request with asyn/await and handle errors and loading state

---
+++ notes.md
@@ -0,0 +1,2 @@
+    When using the fetch function for sending HTTP requests, you can check if the response is the error response
+    response.ok true => // 200, 300 or false =? 400, 500
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,31 +1,57 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
-
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
   // Todo: Fetch available places from backend API
+  // ! When fetching data, its super common to have these 3 pieces of state work together
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   // * Fetch then returns a promise, which is a JS value that eventually resolve to another value. So basically a wrapper object around a value that is not there yet, but that will eventually be there
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        // Now this JSON methods returns anothers promise
-        // console.log(">>>", response.json());
-        return response.json();
-      })
-      .then((resData) => {
-        console.log("resData>>>", resData);
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        // Handle the error and show it to the user
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        });
+        setIsFetching(false);
+      }
+    }
+    fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error accoured" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
