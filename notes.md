ce3798845864efd957c97463cb1bc6500a1d55f9
adnankhan <adnankhannad@gmail.com>
2024-05-05 19:53:11 +0500

    Create and use a custom-hook for fetching data

--- notes.md
+++ notes.md
@@ -1,2 +1,13 @@
     When using the fetch function for sending HTTP requests, you can check if the response is the error response
     response.ok true => // 200, 300 or false =? 400, 500
+
+#### Things we need to do an HTTP request
+
+// Todo: Fetch available places from backend API
+
+// \* Fetch then returns a promise, which is a JS value that eventually resolve to another value. So basically a wrapper object around a value that is not there yet, but that will eventually be there
+
+// ! When fetching data, its super common to have these 3 pieces of state work together
+const [availablePlaces, setAvailablePlaces] = useState([]);
+const [isFetching, setIsFetching] = useState(false);
+const [error, setError] = useState();
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,46 +1,31 @@
-import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
-const places = localStorage.getItem("places");
+import { useFetch } from "../hooks/useFetch.js";
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  // Todo: Fetch available places from backend API
-  // ! When fetching data, its super common to have these 3 pieces of state work together
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
-
-  // * Fetch then returns a promise, which is a JS value that eventually resolve to another value. So basically a wrapper object around a value that is not there yet, but that will eventually be there
-
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-
-      try {
-        const places = await fetchAvailablePlaces();
-
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        // Handle the error and show it to the user
-        setError({
-          message:
-            error.message || "Could not fetch places, please try again later",
-        });
-        setIsFetching(false);
-      }
-    }
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="An error accoured" message={error.message} />;
